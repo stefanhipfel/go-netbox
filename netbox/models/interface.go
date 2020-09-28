fcd4119414a4924256f5e0a8236455df0f66b915
Stefan Hipfel <stefan.hipfel@gmail.com>
2020-09-28 13:43:40 +0200

    Update interface.go

--- netbox/models/interface.go
+++ netbox/models/interface.go
@@ -44,7 +44,7 @@ type Interface struct {
 	// Return the appropriate serializer for the type of connected object.
 	//
 	// Read Only: true
-	ConnectedEndpoint map[string]string `json:"connected_endpoint,omitempty"`
+	ConnectedEndpoint interface{} `json:"connected_endpoint,omitempty"`
 
 	// Connected endpoint type
 	// Read Only: true
